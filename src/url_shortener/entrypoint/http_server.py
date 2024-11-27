dc0be64ca857697ab8e9b469f8e54b6f09d1ad28
Pavel Epanov <pavelepanov@MacBook-Air-Pavel.local>
2024-11-27 17:54:28 +0500

    Add routing in http server

--- src/url_shortener/entrypoint/http_server.py
+++ src/url_shortener/entrypoint/http_server.py
@@ -24,34 +24,48 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         try:
-            self.send_response(200)
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
+            url = urlparse(self.path)
+            path = url.path
 
-            content_length = int(self.headers["Content-Length"])
-            post_data = self.rfile.read(content_length)
-            post_data_str = post_data.decode("utf-8")
+            if path == '/short_url':
+                self.send_response(200)
+                self.send_header("Content-type", "application/json")
+                self.end_headers()
 
-            data = json.loads(post_data_str)
+                content_length = int(self.headers["Content-Length"])
+                post_data = self.rfile.read(content_length)
+                post_data_str = post_data.decode("utf-8")
 
-            response_presenation = asyncio.run(
-                create_short_url(
-                    ioc=self.ioc, request=CreateShortUrlRequest(data["key"])
+                data = json.loads(post_data_str)
+
+                response_presenation = asyncio.run(
+                    create_short_url(
+                        ioc=self.ioc, request=CreateShortUrlRequest(data["key"])
+                    )
                 )
-            )
 
-            post_data_answer = {
-                "id": str(response_presenation.id),
-                "full_url": response_presenation.full_url,
-                "short_url": response_presenation.short_url,
-            }
+                post_data_answer = {
+                    "id": str(response_presenation.id),
+                    "full_url": response_presenation.full_url,
+                    "short_url": response_presenation.short_url,
+                }
 
-            response = {
-                "message": "This is a POST request",
-                "short_url": post_data_answer['short_url'],
-            }
+                response = {
+                    "message": "This is a POST request",
+                    "short_url": post_data_answer['short_url'],
+                }
 
-            self.wfile.write(json.dumps(response).encode("utf-8"))
+                self.wfile.write(json.dumps(response).encode("utf-8"))
+            else:
+                self.send_response(404)
+                response = {
+                    'error': 'Route not found',
+                    'path': path
+                }
+
+                self.send_header('Content-Type', 'application/json')
+                self.end_headers()
+                self.wfile.write(json.dumps(response).encode('utf-8'))
         except Exception as e:
             self.send_response(500)
             self.send_header("Content-Type", "application/json")
@@ -62,23 +76,36 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
             url = urlparse(self.path)
-            query_params = parse_qs(url.query)
-            response_presenation = asyncio.run(
-                get_full_url_by_short_url(
-                    ioc=self.ioc,
-                    request=GetFullUrlByShortUrlRequest(query_params["full_url"][0]),
+            path = url.path
+
+            if path == '/short_url':
+                query_params = parse_qs(url.query)
+                response_presenation = asyncio.run(
+                    get_full_url_by_short_url(
+                        ioc=self.ioc,
+                        request=GetFullUrlByShortUrlRequest(query_params["full_url"][0]),
+                    )
                 )
-            )
 
-            response = {
-                "message": "This is a GET request",
-                "full_url": response_presenation.full_url,
-            }
+                response = {
+                    "message": "This is a GET request",
+                    "full_url": response_presenation.full_url,
+                }
 
-            self.send_response(200)
-            self.send_header("Content-Type", "application/json")
-            self.end_headers()
-            self.wfile.write(json.dumps(response).encode("utf-8"))
+                self.send_response(200)
+                self.send_header("Content-Type", "application/json")
+                self.end_headers()
+                self.wfile.write(json.dumps(response).encode("utf-8"))
+            else:
+                self.send_response(404)
+                response = {
+                    'error': 'Route not found',
+                    'path': path
+                }
+
+                self.send_header('Content-Type', 'application/json')
+                self.end_headers()
+                self.wfile.write(json.dumps(response).encode('utf-8'))
 
         except Exception as e:
             self.send_response(500)
